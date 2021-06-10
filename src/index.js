5f5a05c9d2cdc2b1d4a3f28fdc48851c808a746b
ye win <yewin.aung@zigway.co>
2021-06-10 19:43:05 +0630

    add basic config of webpack & babel

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from "react-dom";
+
+function App() {
+    return (
+        <div>
+            render App
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
